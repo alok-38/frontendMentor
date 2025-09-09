afdaa562fd2104650053759e9a11b2fa8891add4
plantsfortrees@gmail.com <Alokananda Y>
2025-09-09 10:19:20 +0000

    Submitting solution

--- intro-component-with-signup-form-master/app.js
+++ intro-component-with-signup-form-master/app.js
@@ -1,9 +1,16 @@
-const claimButton = document.querySelector('button:not(form button)');
-
 document.addEventListener("DOMContentLoaded", function () {
-    const formEl = document.querySelector('form');
+    const formEl = document.querySelector("form");
+    const claimButton = formEl.querySelector("button");
 
-    formEl.addEventListener('submit', function (e) {
+    claimButton.addEventListener("click", function () {
+        // Optional: formEl.reportValidity(); // shows native validation errors
+        formEl.requestSubmit(); // triggers 'submit' if valid
+    });
+
+    formEl.addEventListener("submit", function (e) {
         e.preventDefault();
-    })
-})
+
+        // Your form handling logic here
+        console.log("Form submitted!");
+    });
+});
