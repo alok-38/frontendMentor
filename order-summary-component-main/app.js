51bf145a96fa2cdb3290b927e59ca8b31cd6004e
plantsfortrees@gmail.com <Alokananda Y>
2025-09-13 08:23:51 +0000

    Submitting solution

--- order-summary-component-main/app.js
+++ order-summary-component-main/app.js
@@ -2,4 +2,26 @@ const cancelButton = document.querySelector('button[type="button"]');
 const paymentButton = document.querySelector('button[type="submit"]');
 const changePlan = document.querySelector('.change-link');
 
+const planPrice = document.querySelector('.plan-price');
+const originalPlanPrice = planPrice.textContent;
 
+const orderSummaryPara = document.querySelector('article:first-of-type p');
+const originalSummaryText = orderSummaryPara.textContent;
+
+// Change to Monthly Plan
+changePlan.addEventListener('click', () => {
+    if (planPrice.textContent !== "$9.99/month") {
+        planPrice.textContent = "$9.99/month";
+    }
+});
+
+// Cancel Order = Reset
+cancelButton.addEventListener('click', () => {
+    planPrice.textContent = originalPlanPrice;
+    orderSummaryPara.textContent = originalSummaryText;
+});
+
+// Payment Confirmation
+paymentButton.addEventListener('click', () => {
+    orderSummaryPara.textContent = "Get grooving!";
+});
