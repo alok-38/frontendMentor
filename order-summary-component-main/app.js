5345860b81f267629e0ae18c15983a7ee65393c1
plantsfortrees@gmail.com <Alokananda Y>
2025-09-13 08:10:56 +0000

    working with JavaScript

---
+++ order-summary-component-main/app.js
@@ -0,0 +1,5 @@
+const cancelButton = document.querySelector('button[type="button"]');
+const paymentButton = document.querySelector('button[type="submit"]');
+const changePlan = document.querySelector('.change-link');
+
+
