93cacc69abda89eb70abf09c0dc7021280d0654c
plantsfortrees@gmail.com <Alokananda Y>
2025-09-18 01:55:56 +0000

    Working with newsletter sign up form

--- JavaScript/managing-forms/app.js
+++ JavaScript/managing-forms/app.js
@@ -2,6 +2,14 @@ const form = document.querySelector('form');
 
 function handleSubmit(e) {
     e.preventDefault();
+
+    const data = {};
+    const fields = e.target.querySelectorAll('input, select, textarea');
+
+    for (const field of fields) {
+        data[field.name] = field.value;
+    }
+    console.log(data);
 }
 
 form.addEventListener('submit', handleSubmit);
