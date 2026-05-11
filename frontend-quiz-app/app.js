fca0e45eafd1fcce25f2ff6c0475dc7e7a00a6f4
Alokananda Y <plantsfortrees@gmail.com>
2026-05-11 12:05:15 +0530

    Adding dark mode functionality

--- frontend-quiz-app/app.js
+++ frontend-quiz-app/app.js
@@ -1,7 +1,14 @@
-const themeToggleSwitch = document.getElementById("theme-toggle");
+// Dark mode functionality
+const themeToggleSwitch = document.getElementById('theme-toggle');
+const quizTopicsList = document.querySelectorAll(".quiz__topic");
+const quizTopicButton = document.querySelectorAll(".quiz__topic-btn");
 
 themeToggleSwitch.addEventListener("click", () => {
-  if (condition) {
-  } else {
-  }
+  quizTopicsList.forEach((list) => {
+    list.style.backgroundColor = "#3b4d66";
+  });
+
+  quizTopicButton.forEach((btn) => {
+    btn.style.color = "white";
+  });
 });
