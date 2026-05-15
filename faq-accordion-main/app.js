6fe425056900820d88cb5f92d3efa6300ad99634
Alokananda Y <plantsfortrees@gmail.com>
2026-05-15 07:47:50 +0530

    Deploying

--- faq-accordion-main/app.js
+++ faq-accordion-main/app.js
@@ -1,7 +1,14 @@
-const buttons = document.querySelectorAll(".faq__question");
-
-buttons.forEach((button) => {
+document.querySelectorAll(".faq__question").forEach((button) => {
   button.addEventListener("click", () => {
-
+    // Extract the number from button ID
+    const questionNumber = button.id.split("-").pop();
+    // Find the corresponding answer
+    const faqAnswer = document.getElementById(`faq-answer-${questionNumber}`);
+    // Toggle visibility
+    const isHidden = faqAnswer.hasAttribute("hidden");
+    if (isHidden) {
+      faqAnswer.removeAttribute("hidden");
+      button.setAttribute("aria-expanded", false);
+    }
   });
 });
