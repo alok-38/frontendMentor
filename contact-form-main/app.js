f66fd5fcfe406db67788be54e630c2da6d4e567d
Alokananda Y <plantsfortrees@gmail.com>
2026-05-16 06:49:17 +0530

    Creating a contact form

---
+++ contact-form-main/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const formGroupErrors = document.querySelectorAll(".form-group__error");
+  let errors;
+  if (!errors) {
+    formGroupErrors.forEach((error) => {
+        
+    })
+  }
+});
