55a0fbe7ddb7d596b2147da9ead12f5cc3b2b7ca
Alokananda Y <plantsfortrees@gmail.com>
2026-05-16 14:39:56 +0530

    Deploying

--- contact-form-main/app.js
+++ contact-form-main/app.js
@@ -1,9 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const formGroupErrors = document.querySelectorAll(".form-group__error");
-  let errors;
-  if (!errors) {
-    formGroupErrors.forEach((error) => {
-        
-    })
-  }
+  const formEl = document.getElementById("contact-form");
+  const errorEls = document.querySelectorAll(".form-group__error");
+  const firstNameInputEl = document.getElementById("first-name");
+  const lastNameInputEl = document.getElementById("last-name");
+  const emailInputEl = document.getElementById("email");
+
+  const firstName = firstNameInputEl.value.trim();
+  const lastName = lastNameInputEl.value.trim();
+  const lastName = lastNameInputEl.value.trim();
+  const email = emailInputEl.value.trim();
+
+  const genralEnquiry = document.getElementById('general-enquiry');
+  const supportRequest = document.getElementById('support-request');
+
+  // Ensure errors are hidden on initial load
+  errorEls.forEach((error) => {
+    error.style.display = "none";
+  });
+
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    errorEls.forEach((error) => {
+      error.style.display = "flex";
+    });
+  });
 });
