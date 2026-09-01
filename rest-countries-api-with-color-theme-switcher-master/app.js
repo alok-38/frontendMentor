a54509ceddad600914bafe419795826d77f668b7
ThePrimeagen <the.primeagen@aol.com>
2026-09-01 09:19:53 +0530

    REST Countries API with color theme switcher

--- rest-countries-api-with-color-theme-switcher-master/app.js
+++
@@ -1,6 +0,0 @@
-import countries from "./data.json" with { type: "json" };
-
-countries.slice(0, 6).forEach((country) => {
-    const mainEl = document.querySelector('main');
-    mainEl.textContent += country.name;
-})
